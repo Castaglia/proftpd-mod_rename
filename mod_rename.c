e095e3ad2ad91255488e97297fac6bbec99777db
TJ Saunders <tj@castaglia.org>
2026-05-30 09:55:39 -0700

    Run the mod_rename integration/regression tests via GitHub workflow.

--- mod_rename.c
+++ mod_rename.c
@@ -1,6 +1,6 @@
 /*
  * ProFTPD: mod_rename -- a module for automatically renaming uploaded files
- * Copyright (c) 2001-2017 TJ Saunders
+ * Copyright (c) 2001-2026 TJ Saunders
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -13,8 +13,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ * along with this program; if not, see <https://www.gnu.org/licenses/>.
  *
  * As a special exemption, TJ Saunders gives permission to link this program
  * with OpenSSL, and distribute the resulting executable, without including
